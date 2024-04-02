32f554045964e69a87a9f3d78fdb960f67d4c21d
Javatrix <javatrix912@gmail.com>
2024-04-02 14:50:34 +0100

    Colored window.

--- src/main.cpp
+++ src/main.cpp
@@ -15,6 +15,9 @@ int main(int argc, char *argv[]) {
 
   glfwSetErrorCallback(glfwError);
 
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
   GLFWwindow *window =
       glfwCreateWindow(640, 480, "An amazing GLFW example", NULL, NULL);
   if (!window) {
@@ -23,7 +26,9 @@ int main(int argc, char *argv[]) {
   }
 
   glfwMakeContextCurrent(window);
+  glClearColor(1, 0, 1, 1);
   while (!glfwWindowShouldClose(window)) {
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
     glfwPollEvents();
     glfwSwapBuffers(window);
   }
