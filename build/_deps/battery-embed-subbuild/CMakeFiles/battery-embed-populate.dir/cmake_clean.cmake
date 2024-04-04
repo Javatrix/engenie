4e1316511d5c6997178cb2db82fac81b9bcff665
Javatrix <javatrix912@gmail.com>
2024-04-04 18:32:58 +0100

    Include embed files support.

---
+++ build/_deps/battery-embed-subbuild/CMakeFiles/battery-embed-populate.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/battery-embed-populate"
+  "CMakeFiles/battery-embed-populate-complete"
+  "battery-embed-populate-prefix/src/battery-embed-populate-stamp/battery-embed-populate-build"
+  "battery-embed-populate-prefix/src/battery-embed-populate-stamp/battery-embed-populate-configure"
+  "battery-embed-populate-prefix/src/battery-embed-populate-stamp/battery-embed-populate-download"
+  "battery-embed-populate-prefix/src/battery-embed-populate-stamp/battery-embed-populate-install"
+  "battery-embed-populate-prefix/src/battery-embed-populate-stamp/battery-embed-populate-mkdir"
+  "battery-embed-populate-prefix/src/battery-embed-populate-stamp/battery-embed-populate-patch"
+  "battery-embed-populate-prefix/src/battery-embed-populate-stamp/battery-embed-populate-test"
+  "battery-embed-populate-prefix/src/battery-embed-populate-stamp/battery-embed-populate-update"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/battery-embed-populate.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/_deps/battery-embed-subbuild/CMakeFiles/battery-embed-populate.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty custom commands generated dependencies file for battery-embed-populate.
+# This may be replaced when dependencies are built.
